c27559297d6f633a8a936420dce3bee5fd36d184
israelcamp <israelcampiotti@gmail.com>
2020-01-17 07:16:44 -0300

    minor fixes

--- h_morphological.m
+++ h_morphological.m
@@ -6,6 +6,6 @@ H = zeros(num_examples,num_neurons);
 for j = 1:num_examples
    e = min(X(j,:)' + V);
    d = min(-X(j,:)' + W);
-   H(j,:) = min(min(e,d), zeros(size(e)));
+   H(j,:) = max(min(e,d), zeros(size(e)));
 %    H(j,:) = min(e,d);
 end
